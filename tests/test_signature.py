5ca057dd66733b389cba48bf6de99b2fb13f8001
dgpetrie <dpetrie@sipez.com>
2022-06-07 02:18:48 +0000

    Code and minimal unit tests for RFC7515 JWS signing and verification of vCons

---
+++ tests/test_signature.py
@@ -0,0 +1,296 @@
+""" unit test related to RFC7515 JSON Web Signature (JWS) functionality """
+
+import typing
+import pytest
+import vcon
+import vcon.security
+import json
+import pprint
+import cryptography.x509
+import base64
+
+import jose.jwk
+import jose.jws
+import datetime
+
+CA_CERT = "certs/fake_ca_root.crt"
+CA2_CERT = "certs/fake_ca2_root.crt"
+EXPIRED_CERT = "certs/expired_div.crt"
+DIVISION_CERT = "certs/fake_div.crt"
+DIVISION_PRIVATE_KEY = "certs/fake_div.key"
+GROUP_CERT = "certs/fake_grp.crt"
+GROUP_PRIVATE_KEY = "certs/fake_grp.key"
+
+call_data = {
+      "epoch" : "1652552179",
+      "destination" : "2117",
+      "source" : "+19144345359",
+      "rfc2822" : "Sat, 14 May 2022 18:16:19 -0000",
+      "file_extension" : "WAV",
+      "duration" : 94.84,
+      "channels" : 1
+}
+
+@pytest.fixture(scope="function")
+def empty_vcon() -> vcon.Vcon:
+  """ construct vCon with no data """
+  vCon = vcon.Vcon()
+  return(vCon)
+
+@pytest.fixture(scope="function")
+def two_party_tel_vcon(empty_vcon : vcon.Vcon) -> vcon.Vcon:
+  """ construct vCon with two tel URL """
+  vCon = empty_vcon
+  first_party = vCon.set_party_tel_url(call_data['source'])
+  second_party = vCon.set_party_tel_url(call_data['destination'])
+  return(vCon)
+
+@pytest.fixture()
+def ca_cert() -> typing.Tuple[cryptography.x509.Certificate, str]:
+  """ load certificate of authority (issuer of Division cert) X.509 certificate and public key from file and return the string """
+  return(vcon.security.load_pem_cert(CA_CERT))
+
+@pytest.fixture()
+def ca2_cert() -> typing.Tuple[cryptography.x509.Certificate, str]:
+  """ load certificate of 2nd authority (issuer of Division cert) X.509 certificate and public key from file and return the string """
+  return(vcon.security.load_pem_cert(CA2_CERT))
+
+@pytest.fixture()
+def div_cert() -> typing.Tuple[cryptography.x509.Certificate, str]:
+  """ load Division X.509 certificate and public key from file and return the string """
+  return(vcon.security.load_pem_cert(DIVISION_CERT))
+
+@pytest.fixture()
+def expired_cert() -> typing.Tuple[cryptography.x509.Certificate, str]:
+  """ load expired Division X.509 certificate and public key from file and return the string """
+  return(vcon.security.load_pem_cert(EXPIRED_CERT))
+
+@pytest.fixture()
+def div_key() -> cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey:
+  """ load Division X.509 private key from file and return the string """
+  key_object = vcon.security.load_pem_key(DIVISION_PRIVATE_KEY)
+
+  return(key_object)
+
+@pytest.fixture()
+def good_x5c_list() -> typing.List[str]:
+  file_names = [GROUP_CERT, DIVISION_CERT, CA_CERT]
+  c5x = vcon.security.load_x5c_from_pem_certs(file_names)
+  return(c5x)
+
+@pytest.fixture()
+def invalid_x5c_list() -> typing.List[str]:
+  file_names = [GROUP_CERT, EXPIRED_CERT, CA_CERT]
+  c5x = vcon.security.load_x5c_from_pem_certs(file_names)
+  return(c5x)
+
+# Not sure this interface is right or needed
+def build_verification_jwk_from_file(cert_chain_der_strings : typing.List[str], ca_pem_strings : typing.List[str]) -> dict:
+   """
+   Read DER strings for certificate chain and construct a JWS for signed verification.
+   Verify the cert chain is linked to one of the certficate authorites in the list provided.
+
+   Returns:
+     JWK (dict): key for JWS verification of signed JWS
+   """
+
+def test_key(div_cert: typing.Tuple[cryptography.x509.Certificate, str], div_key: cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey) -> None:
+
+  cert_object = div_cert[0]
+  print("cert_object type: {}".format(type(cert_object)))
+  cert_der = div_cert[1]
+  print(cert_der)
+
+  private_key_object = div_key
+  # cert_object is a cryptography.x509.Certificate??
+  print("cert_object type: {}".format(type(cert_object)))
+  print("cert_object dir: {}".format(dir(cert_object.public_key())))
+  print("cert_object valid start: {}".format(cert_object.not_valid_before))
+  print("cert_object valid end: {}".format(cert_object.not_valid_after))
+  print("cert_object pub nums dir: {}".format(dir(cert_object.public_key().public_numbers())))
+  print("cert_object pub key dir: {}".format((cert_object.public_key().public_numbers().public_key)))
+  print("cert_object n: {}".format(jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().n))))
+  print("cert_object e: {}".format(jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().e))))
+  #print("cert_object dir: {}".format(dir(cert_object)))
+  #print("cert_object issuer: {}".format(cert_object.issuer.rfc4514_string()))
+  #print("cert_object subject: {}".format(cert_object.subject.rfc4514_string()))
+
+  # subject is a cryptography.x509.name.Name
+  # field is a cryptography.x509.name.NameAttribute
+  for field in cert_object.subject:
+    #print(dir(field.oid))
+    print("subject {:20} {:20} {:22} {}".format(field.rfc4514_attribute_name, field.oid.dotted_string, field.oid._name, field.value))
+    #         RFC4514 Name         ASN.1 OID            OID Name               Example Value
+    # subject C                    2.5.4.6              countryName            US
+    # subject ST                   2.5.4.8              stateOrProvinceName    MA
+    # subject L                    2.5.4.7              localityName           Faketown
+    # subject O                    2.5.4.10             organizationName       FakeVcon
+    # subject OU                   2.5.4.11             organizationalUnitName Division
+    # subject CN                   2.5.4.3              commonName             div.fakevcon.io
+    # subject 1.2.840.113549.1.9.1 1.2.840.113549.1.9.1 emailAddress           admin@fakevcon.org
+    # subject 2.5.29.17            2.5.29.17            subjectAltName         div.fakevcon.org
+
+  for field in cert_object.issuer:
+    print("issuer {:20} {:20} {:22} {}".format(field.rfc4514_attribute_name, field.oid.dotted_string, field.oid._name, field.value))
+
+  algorithm = "RS256"
+  #crypt_key = jose.jwk.construct(cert_object.public_key(), algorithm)
+
+  #print(crypt_key)
+  #print(dir(crypt_key))
+
+  signing_key = {}
+  signing_key["kty"] = "RSA"
+  signing_key["use"] = "sig"
+  signing_key["alg"] = algorithm
+  signing_key["n"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().n)).decode('utf-8')
+  signing_key["e"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().e)).decode('utf-8')
+  signing_key["d"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().d)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # (p,p) = cryptography.hazmat.primitives.asymmetric.rsa.rsa_recover_prime_factors(n, e, d)
+  signing_key["p"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().p)).decode('utf-8')
+  signing_key["q"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().q)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_dmp1(d, p)
+  signing_key["dp"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().dmp1)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_dmq1(d, q)
+  signing_key["dq"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().dmq1)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_iqmp(p,q)
+  signing_key["qi"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().iqmp)).decode('utf-8')
+
+  print("signing key:")
+  pprint.pprint(signing_key)
+
+  verification_key = {}
+  verification_key["kty"] = "RSA"
+  verification_key["use"] = "sig"
+  verification_key["alg"] = algorithm
+  verification_key["n"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().n)).decode('utf-8')
+  verification_key["e"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().e)).decode('utf-8')
+  #jwk["e"] = "AQAB"
+
+  header = {}
+  header["x5c"] = [ cert_der ]
+  print("\nverification key:")
+  pprint.pprint(verification_key)
+
+  #x5c_key = jose.jwk.construct(jwk, algorithm=algorithm)
+  payload = { "mything" : "data to sign"}
+  #jws_token = x5c_key.sign(payload)
+  jws_token = jose.jws.sign(payload, signing_key, algorithm=signing_key["alg"])
+
+  print(jws_token)
+
+  verification_key["n"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().n)).decode('utf-8')
+  verified_payload = jose.jws.verify(jws_token, verification_key, algorithm)
+  print("verified: {}".format(verified_payload))
+
+def test_verify_cert(div_cert : typing.Tuple[cryptography.x509.Certificate, str],
+  ca_cert : typing.Tuple[cryptography.x509.Certificate, str]) -> None:
+  cert_to_verify, cert_der =  div_cert
+  issuer_cert, issuer_der = ca_cert
+
+  vcon.security.verify_cert(cert_to_verify, issuer_cert)
+
+def test_verify_expired_cert(expired_cert : typing.Tuple[cryptography.x509.Certificate, str],
+  ca_cert : typing.Tuple[cryptography.x509.Certificate, str]) -> None:
+  cert_to_verify, cert_der =  expired_cert
+  issuer_cert, issuer_der = ca_cert
+
+  try:
+    vcon.security.verify_cert(cert_to_verify, issuer_cert)
+    raise Exception("Should have thrown  exception for expired cert")
+
+  except vcon.security.InvalidCertDate as invalid_date_e:
+    pass
+
+def test_verify_ca_ca(ca_cert : typing.Tuple[cryptography.x509.Certificate, str]) -> None:
+  """ ca is self signed, so technically it can be used to verify itself """
+  ca1_cert = ca_cert[0]
+  ca2_cert = ca_cert[0]
+
+  vcon.security.verify_cert(ca1_cert, ca2_cert)
+
+def test_verify_wrong_cert(div_cert : typing.Tuple[cryptography.x509.Certificate, str],
+  ca2_cert : typing.Tuple[cryptography.x509.Certificate, str]) -> None:
+  cert_to_verify, cert_der =  div_cert
+  issuer_cert, issuer_der = ca2_cert
+
+  try:
+    vcon.security.verify_cert(cert_to_verify, issuer_cert)
+    raise Exception("Should have thrown  exception for invalid signature")
+
+  except cryptography.exceptions.InvalidSignature as invalid_sig:
+    # Should get her because this CA did not sign the cert being verified
+    pass
+
+def test_cert_chain(good_x5c_list : typing.List[str]) -> None:
+  c5x_list = good_x5c_list
+  cert_chain = vcon.security.der_to_certs(c5x_list)
+
+  vcon.security.verify_cert_chain(cert_chain)
+
+def test_invalid_cert_chain(invalid_x5c_list : typing.List[str]) -> None:
+  c5x_list = invalid_x5c_list
+  cert_chain = vcon.security.der_to_certs(c5x_list)
+
+  try:
+    vcon.security.verify_cert_chain(cert_chain)
+    raise Exception("SHould have caught invalid cert at element 1 in chain")
+
+  # Should raise this exception:
+  except cryptography.exceptions.InvalidSignature as invalid_error:
+    pass
+
+def test_expired_cert_chain(expired_cert : typing.Tuple[cryptography.x509.Certificate, str], ca_cert : typing.Tuple[cryptography.x509.Certificate, str]) -> None:
+  c5x_list = [expired_cert[1], ca_cert[1]]
+  cert_chain = vcon.security.der_to_certs(c5x_list)
+
+  try:
+    vcon.security.verify_cert_chain(cert_chain)
+    raise Exception("Should have raise exceptin for invalid date on cert in element 0 of chain")
+
+  # Should raise this exception:
+  except vcon.security.InvalidCertDate as invalid_error:
+    pass
+
+def test_sign_vcon(two_party_tel_vcon : vcon.Vcon) -> None:
+  two_party_tel_vcon.sign(GROUP_PRIVATE_KEY, [GROUP_CERT, DIVISION_CERT, CA_CERT])
+
+  try:
+    two_party_tel_vcon.verify([CA_CERT])
+    raise Exception("Should have thrown an exception as this vcon was signed locally")
+
+  except vcon.InvalidVconState as already_signed_error:
+    # Expected to get here because vCon was signed locally
+    # Its already verified
+    pass
+
+  vcon_json = two_party_tel_vcon.dumps()
+  #print("Signed vcon: {}".format(vcon_json))
+
+  deserialized_signed_vcon = vcon.Vcon()
+  deserialized_signed_vcon.loads(vcon_json)
+
+  vcon_json2 = deserialized_signed_vcon.dumps()
+  assert(vcon_json == vcon_json2)
+
+  try:
+    party_count = len(deserialized_signed_vcon.parties)
+    raise Exception("Should not get here.  Vcon is signed, but not yet verified.  Cannot access data.")
+
+  except vcon.UnverifiedVcon as unverified_error:
+    # Should get here
+    pass
+
+  deserialized_signed_vcon.verify([CA_CERT])
+  assert(len(deserialized_signed_vcon.parties) == 2)
+  assert(deserialized_signed_vcon.parties[0]['tel'] == call_data['source'])
+  assert(deserialized_signed_vcon.parties[1]['tel'] == call_data['destination'])
+
---
+++ vcon/security.py
@@ -0,0 +1,240 @@
+""" security helper functions for the Vcon package """
+
+import typing
+import cryptography.hazmat.backends.openssl.backend
+import cryptography.x509
+#import re
+import base64
+import jose
+import datetime
+
+class InvalidCertDate(Exception):
+  """ Cert not_valid_before or not_valid_after dates don't include today """
+
+def load_string_from_file(file_name : str):
+  file_contents_string = None
+  with open(file_name, 'r') as file_handle:
+    file_contents_string = file_handle.read()
+
+  return(file_contents_string)
+
+def load_pem_cert(cert_file : str) -> typing.Tuple[cryptography.x509.Certificate, str]:
+  """
+  Load PEM formate certificate containing public key and construct cert object and DER representation of PEM file.
+
+  Returns:
+    Tuple(cert_object, str): cert object and DER string
+  """
+  cert_string = load_string_from_file(cert_file)
+
+  # Need the base64 encoded cert with no header, footer or white space for the x5c field in the key
+  #bytes_start = cert_string.find('-\n') + 1
+  #bytes_end = cert_string.find('\n-', bytes_start)
+  #print("PEM header end: {} foot start: {}".format(bytes_start, bytes_end))
+  # Pulls only the first encoded PEM object in the string
+  #cert_no_begin_end = cert_string[bytes_start : bytes_end]
+
+  # basically the DER format of the PEM cert
+  #cert_no_whitespace = re.sub(r"\s", "", cert_no_begin_end)
+  #print(cert_no_whitespace)
+
+  # Import in PEM format
+  cert_object = cryptography.x509.load_pem_x509_certificate(bytes(cert_string, 'utf-8'), cryptography.hazmat.backends.openssl.backend)
+  # Alternatively load DER form:
+  #cert_object = cryptography.x509.load_der_x509_certificate(base64.b64decode(div_cert_no_whitespace), cryptography.hazmat.backends.openssl.backend)
+  der = base64.b64encode(cert_object.public_bytes(cryptography.hazmat.primitives.serialization.Encoding.DER)).decode('utf-8')
+  #, cryptography.hazmat.primitives.serialization.PublicFormat.SubjectPublicKeyInfo)
+  #print("DER: {}".format(der))
+  #assert(der == cert_no_whitespace)
+
+  return(cert_object, der)
+
+def load_x5c_from_pem_certs(cert_pem_file_names : typing.List[str]) -> typing.List[str]:
+  """
+  Construct a x5c compatible list (RFC7515,RFC7517,RFC7518) from certifcate PEM files.
+
+  Parameters:
+    cert_pem_file_names (List[str]): list of PEM file names representing the certificate chain
+             sorted in order from the signing cert for the JWS/JWE to the certificate authority
+             root of the chain.
+
+  Returns:
+    List(str): The certifcate chain in DER format campatible with the c5c parameter in the
+             above reference RFCs.
+
+  """
+
+  c5x_list = []
+  for file_name in cert_pem_file_names:
+    der_string = load_pem_cert(file_name)[1]
+    c5x_list.append(der_string)
+
+  return(c5x_list)
+
+def der_to_certs(x5c : typing.List[str]) -> typing.List[cryptography.x509.Certificate]:
+  """
+  Construct the cryptograpy certifate objects for the list of DER format certificate strings
+
+  Parameters:
+    x5c (List[str]): list containing DER foramt cert strings (e.g. RFC7517)
+
+  Returns:
+    List of certificate object for each DER passed in.
+  """
+  cert_list = []
+  for der in x5c:
+    cert_object = cryptography.x509.load_der_x509_certificate(base64.b64decode(der), cryptography.hazmat.backends.openssl.backend)
+    cert_list.append(cert_object)
+
+  return(cert_list)
+
+def load_pem_key(key_file_name : str) -> cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey:
+  """
+  Load PEM format private key and construct private key object
+
+  Returns:
+    Tuple(cert_object, str): cert object and DER string
+  """
+  pem_key_string = load_string_from_file(key_file_name)
+
+  # cryptography.x509.load_pem_x509_private_key does not exist.  So we much wade through hazmat
+  private_key_object = cryptography.hazmat.primitives.serialization.load_pem_private_key(bytes(pem_key_string, 'utf-8'), None)
+  #print("private_key type: {}".format(type(private_key_object)))
+  #print("private dir {}".format(dir(private_key_object.private_numbers())))
+
+  return(private_key_object)
+
+def build_signing_jwk_from_pem_files(private_key_pem_file_name : str, cert_chain_pem_file_names : typing.List[str]) -> typing.Tuple[dict, dict]:
+  """
+  Read PEM files for pricate key and certificate chain all in PEM format and construct header and JWK for signing
+
+  Parameters:
+    private_key_pem_file_name (str): the private key to use for signing the vcon.
+
+    cert_chain_pem_file_names (List{str]): file names for the pem format certicate chain for the
+      private key to use for signing.  The cert/public key corresponding to the private key should be the
+      first cert.  THe certificate authority root should be the last cert.
+
+  Returns:
+    Tuple[dict, dict]: header, JWK
+        - header appropriate for including in JWS including x5c and alg
+        - JWK including private key info for signing a JWS
+
+  """
+  # Load the cert cahin into a x5c compatible array
+  x5c = load_x5c_from_pem_certs(cert_chain_pem_file_names)
+
+  algorithm = "RS256"
+
+  header = {}
+  header['x5c'] = x5c
+  header["alg"] = algorithm
+
+  private_key_object = load_pem_key(private_key_pem_file_name)
+
+  # Only need the public key/object for the first/signing cert
+  cert_object = der_to_certs([x5c[0]])[0]
+
+  signing_key = {}
+  signing_key["kty"] = "RSA"
+  signing_key["use"] = "sig"
+  signing_key["alg"] = algorithm
+  signing_key["n"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().n)).decode('utf-8')
+  signing_key["e"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_object.public_key().public_numbers().e)).decode('utf-8')
+  signing_key["d"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().d)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # (p,p) = cryptography.hazmat.primitives.asymmetric.rsa.rsa_recover_prime_factors(n, e, d)
+  signing_key["p"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().p)).decode('utf-8')
+  signing_key["q"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().q)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_dmp1(d, p)
+  signing_key["dp"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().dmp1)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_dmq1(d, q)
+  signing_key["dq"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().dmq1)).decode('utf-8')
+
+  # if missing, can be computed using:
+  # cryptography.hazmat.primitives.asymmetric.rsa.rsa_crt_iqmp(p,q)
+  signing_key["qi"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(private_key_object.private_numbers().iqmp)).decode('utf-8')
+
+  return(header, signing_key)
+
+def verify_cert_chain(cert_chain : typing.List[cryptography.x509.Certificate]) -> None:
+  """
+  Verify the chain of certs validate the preseeding cert in the list and that the dates are valid.
+
+  Parameters:
+    cert_chain (Certicate): the following cert should be the issuer for the preceding cert
+
+  Raises invalid cert or date as found.
+  """
+
+  # TODO: The certificate and certificate chain verification needs a whole lot more
+  #       scrutany and unit testing.  Should Get 100% coverage.
+
+  if(len(cert_chain) < 2):
+    raise AttributeError("No CA. Cert chain must contain at least 2 certs")
+
+  # TODO: should the root CA be used to verify itself???
+
+  for index, issuer_cert in enumerate(cert_chain):
+    # Skip the first cert, its the cert used for signing or encryption.
+    # Its not an issuer.
+    if(index > 0):
+      #print("verifying cert: {}".format(index - 1))
+      verify_cert(cert_to_verify, issuer_cert)
+
+    cert_to_verify = issuer_cert
+
+def verify_cert(cert_to_verify : cryptography.x509.Certificate, issuer_cert : cryptography.x509.Certificate) -> None:
+  """
+  Verify the signature of the given cert matches that of the issuer cert.
+
+  Parameters:
+    cert_to_verify (Certifcate): cert on which to verify the signature and dates
+    issuer_cert (Certifcate): cert which should be the signer/issuer of the cert_to_verify
+
+  Raises exceptions for invalid signature or date on the cert to verify.
+  """
+  issuer_cert.public_key().verify(
+    cert_to_verify.signature,
+    cert_to_verify.tbs_certificate_bytes,
+    cryptography.hazmat.primitives.asymmetric.padding.PKCS1v15(),
+    cert_to_verify.signature_hash_algorithm)
+
+  # check dates on certs
+  now = datetime.datetime.today()
+
+  if(now < cert_to_verify.not_valid_before):
+    name = "None"
+    alt_name = "None"
+    try:
+      name = cert_to_verify.subject.get_attributes_for_oid(cryptography.x509.NameOID.COMMON_NAME)[0].value
+      alt_name = cert_to_verify.subject.get_attributes_for_oid(cryptography.x509.ExtensionOID.SUBJECT_ALTERNATIVE_NAME)[0].value
+    except Exception as e:
+      # REMOVE THIS RAISE after testing
+      raise e
+
+    raise InvalidCertDate("certificate with name: {} altname: {} is not valid until: {}".format(
+      name, alt_name,
+      cert_to_verify.not_valid_before))
+
+  if(now > cert_to_verify.not_valid_after):
+    name = "None"
+    alt_name = "None"
+    try:
+      name = cert_to_verify.subject.get_attributes_for_oid(cryptography.x509.NameOID.COMMON_NAME)[0].value
+      alt_name = cert_to_verify.subject.get_attributes_for_oid(cryptography.x509.ExtensionOID.SUBJECT_ALTERNATIVE_NAME)[0].value
+    except Exception as e:
+      # REMOVE THIS RAISE after testing
+      raise e
+
+    raise InvalidCertDate("certificate with name: {} altname: {} is not valid after: {}".format(
+      name, alt_name,
+      cert_to_verify.not_valid_before))
+
+  # TODO need to check revokations as well
+

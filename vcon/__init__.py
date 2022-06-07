52da6a27d9cf8432c3781e70bfb5126a4e2a1a11
dgpetrie <dpetrie@sipez.com>
2022-06-07 02:21:12 +0000

    The other part of the vCon signing and verification code that did not get commited.

--- vcon/__init__.py
+++ vcon/__init__.py
@@ -5,9 +5,27 @@ see https:/vcon.dev
 import typing
 import json
 import jose.utils
+import enum
+
+class VconStates(enum.Enum):
+  """ Vcon states WRT signing and verification """
+  UNKNOWN = 0
+  UNSIGNED = 1
+  SIGNED = 2
+  UNVERIFIED = 3
+  VERIFIED = 4
 
 class UnsupportedVconVersion(Exception):
   """ Thrown if vcon version string is not of set of versions supported by this package"""
+
+class UnverifiedVcon(Exception):
+  """ Payload is signed, but not verified.  Must be verified before reading data """
+
+class InvalidVconState(Exception):
+  """ Vcon is in an invalid state for a given operation """
+
+class InvalidVconJson(Exception):
+  """ JSON not valid for Vcon """
 
 class VconDictList:
   """ descriptor for Lists of dicts in vcon """
@@ -18,6 +36,10 @@ class VconDictList:
   def __get__(self, instance_object, class_type = None) -> list:
     #print("getting: {} inst type: {} class type: {}".format(self.name, type(instance_object), type(class_type)))
     # TODO: once signed, this should return a read only list
+
+    if(instance_object._state == VconStates.UNVERIFIED):
+      raise UnverifiedVcon("vCon is signed, but not verified. Call verify before reading data.")
+
     return(instance_object._vcon_dict.get(self.name, None))
 
   def __set__(self, instance_object, value : dict) -> None:
@@ -75,6 +97,9 @@ class Vcon():
   """
 
   def __init__(self):
+    self._state = VconStates.UNSIGNED
+    self._jws_dict = None
+
     self._vcon_dict = {}
     self._vcon_dict[Vcon.VCON_VERSION] = "0.0.1"
     self._vcon_dict[Vcon.PARTIES] = []
@@ -192,6 +217,15 @@ class Vcon():
     return(len(body))
 
   def decode_dialog_inline_recording(self, dialog_index : int) -> bytes:
+    """
+    Get the dialog recording at the given index, decoding it and returning the raw bytes.
+
+    Parameters:
+      dialog_index (int): index the the dialog in the dialog list, containing the inline recording
+
+    Returns:
+      (bytes): the bytes for the recording file
+    """
     dialog = self.dialog[dialog_index]
     if(dialog["type"] != "recording"):
       raise AttributeError("dialog[{}] is not a recording file")
@@ -267,7 +301,13 @@ class Vcon():
     # TODO: Should it throw an acception if its not signed?  Could have argument to
     # not throw if it not signed.
 
-    return(json.dumps(self._vcon_dict))
+    if(self._state == VconStates.UNSIGNED):
+      return(json.dumps(self._vcon_dict))
+
+    if(self._state in [VconStates.SIGNED, VconStates.UNVERIFIED, VconStates.VERIFIED]):
+      return(json.dumps(self._jws_dict))
+
+    raise InvalidVconState("vCon state: {} is not valid for dumps".format(self._state))
 
   def loads(self, vcon_json : str) -> None:
     """
@@ -282,23 +322,181 @@ class Vcon():
 
     #TODO: Should check unsafe stuff is not loaded
 
-    #TODO: Once signing is supported, this will get more complicated as
-    #      we will need to check the format as to whether it is signed or
-    #      not and deconstruct the loaded object.
-
-    # TODO: load differently based upon the contents of the JSON
     """
     Decision as to what json to be deserialized is:
     1) vcon must have a vcon and one or more of the following elements: parties, dialog, analysis, attachments
     2) JWS must have a payload and signatures
     3) JWE must have a protected and recipients
     """
+
+    if(self._state != VconStates.UNSIGNED):
+      raise InvalidVconState("Cannot load Vcon unless current state is UNSIGNED.  Current state: {}".format(self._state))
+
     vcon_dict = json.loads(vcon_json)
 
-    # validate version
-    version_string = vcon_dict.get('vcon', "not set")
-    if(version_string != "0.0.1"):
-      raise UnsupportedVconVersion("loads of JSON vcon version: \"{}\" not supported".format(version_string))
+    # we need to check the format as to whether it is signed or
+    # not and deconstruct the loaded object.
+    # load differently based upon the contents of the JSON
 
-    self._vcon_dict = vcon_dict
+    # Signed vCon (JWS)
+    if(("payload" in vcon_dict) and
+      ("signatures" in vcon_dict)
+      ):
+      self._vcon_dict = {}
+
+      self._state = VconStates.UNVERIFIED
+      self._jws_dict = vcon_dict
+
+    # Unsigned vCon has to have vcon version and
+    elif(('vcon' in vcon_dict) and (
+      # one of the following arrays
+      ('parties' in vcon_dict) or
+      ('dialog' in vcon_dict) or
+      ('analysis' in vcon_dict) or
+      ('attachments' in vcon_dict)
+      )):
+
+      # validate version
+      version_string = vcon_dict.get('vcon', "not set")
+      if(version_string != "0.0.1"):
+        raise UnsupportedVconVersion("loads of JSON vcon version: \"{}\" not supported".format(version_string))
+
+      self._vcon_dict = vcon_dict
+
+    # Unknown
+    else:
+      raise InvalidVconJson("Not recognized as a unsigned or signed JSON vCon")
+
+
+  def sign(self, private_key_pem_file_name : str, cert_chain_pem_file_names : typing.List[str]) -> None:
+    """
+    Sign the vcon using the given private key from the give certificate chain.
+
+    Parameters:
+      cert_chain_pem_file_names (List{str]): file names for the pem format certicate chain for the
+        private key to use for signing.  The cert/public key corresponding to the private key should be the
+        first cert.  THe certificate authority root should be the last cert.
+
+    private_key_pem_file_name (str): the private key to use for signing the vcon.
+
+    Returns: none
+    """
+
+    header, signing_jwk = security.build_signing_jwk_from_pem_files(private_key_pem_file_name, cert_chain_pem_file_names)
+
+    # dot separated JWS token.  First part is the payload, second part is the signature (both base64url encoded)
+    jws_token = jose.jws.sign(self._vcon_dict, signing_jwk, headers=header, algorithm=signing_jwk["alg"])
+    #print(jws_token.split('.'))
+    protected_header, payload, signature = jws_token.split('.')
+    #print("decoded header: {}".format(jose.utils.base64url_decode(bytes(protected_header, 'utf-8'))))
+
+    jws_serialization = {}
+    jws_serialization['payload'] = payload
+    jws_serialization['signatures'] = []
+    first_sig = {}
+    first_sig['header'] = header
+    first_sig['signature'] = signature
+    first_sig['protected'] = protected_header
+    jws_serialization['signatures'].append(first_sig)
+
+    self._jws_dict = jws_serialization
+    self._state = VconStates.SIGNED
+
+  def verify(self, ca_cert_pem_file_names : typing.List[str]) -> None:
+    """
+    Verify the signed vCon and its certificate chain which should be issued by one of the given CAs
+
+    Parameters:
+      ca_cert_pem_file_names (List[str]): list of Certificate Authority certificate PEM file names
+        to verify the vCon's certificate chain.
+
+    Returns: none
+
+    Raises exceptions for invalid cert chaind, invalid cert dates or chain not issued by one
+    of the given CAs.
+
+    NOTE:  DOES NOT CHECK REVOKATION LISTS!!!
+    """
+    if(self._state == VconStates.SIGNED):
+      raise InvalidVconState("Vcon was locally signed.  No need to verify")
+
+    if(self._state == VconStates.VERIFIED):
+      raise InvalidVconState("Vcon was already verified")
+
+    if(self._state != VconStates.UNVERIFIED):
+      raise InvalidVconState("Vcon cannot be verified invalid state: {}")
+
+    if(self._jws_dict is None or
+      ('signatures' not in self._jws_dict) or
+      (len(self._jws_dict['signatures']) < 1) or
+      ('signature' not in self._jws_dict['signatures'][0])
+      ):
+      raise InvalidVconState("Vcon JWS invalid")
+
+    # Load an array of CA certficate objects to use to verify acceptable cert chains
+    ca_cert_object_list = []
+    for ca in ca_cert_pem_file_names:
+      ca_cert_object_list.append(security.load_pem_cert(ca)[0])
+
+    last_exception = Exception("Internal error in Vcon.verify this exception should never be thrown")
+    chain_count = 0
+    for signature in self._jws_dict['signatures']:
+      if('header' in signature):
+        if('x5c' in signature['header']):
+          x5c = signature['header']['x5c']
+          chain_count += 1
+
+          cert_chain_objects = security.der_to_certs(x5c)
+
+          # TODO: some of this should be move to the security submodule
+          # e.g. the iterating over CAs
+
+          # TODO: need to do something a little smarter on the exception raise to
+          # give a clue of the best/closest chain and CA that failed.  Perhaps
+          # even all of the failures.
+
+          try:
+            security.verify_cert_chain(cert_chain_objects)
+
+            # We have a valid chain, check if its from one of the accepted CAs
+            for ca_object in ca_cert_object_list:
+              try:
+                security.verify_cert(cert_chain_objects[len(cert_chain_objects) - 1], ca_object)
+
+                # IF we get here, we have a valid chain: cert_chain_objects issued from one of our accepted
+                # CAs: ca_object.
+                # The assumtion is that it is safe to trust this cert chain.  So we
+                # can use it to build a JWK and verify the signature.
+                verification_jwk = {}
+                verification_jwk["kty"] = "RSA"
+                verification_jwk["use"] = "sig"
+                verification_jwk["alg"] = signature['header']['alg']
+                verification_jwk["e"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_chain_objects[0].public_key().public_numbers().e)).decode('utf-8')
+                verification_jwk["n"] = jose.utils.base64url_encode(jose.utils.long_to_bytes(cert_chain_objects[0].public_key().public_numbers().n)).decode('utf-8')
+
+                jws_token = signature['protected'] + "." + self._jws_dict['payload'] + "." + signature['signature']
+                verified_payload = jose.jws.verify(jws_token, verification_jwk, verification_jwk["alg"])
+
+                # If we get here, the payload was verified
+                #print("verified payload: {}".format(verified_payload))
+                #print("verified payload type: {}".format(type(verified_payload)))
+                self._vcon_dict = json.loads(verified_payload.decode('utf-8'))
+                self._state = VconStates.VERIFIED
+
+                return(None)
+
+              # This valid chain, is not issued from the CA for this ca_objjwk
+              except Exception as e: 
+                last_exception = e
+                # Keep trying other CAs until we run out or succeed
+
+          # Invalid chain
+          except Exception as e:
+            last_exception = e
+            # Keep trying other chains until we run out or succeed
+
+    if(chain_count == 0):
+      raise InvalidVconSignature("None of the signatures contain the x5c chain, which this implementation currenlty requires.")
+
+    raise last_exception
 

d9e1fc6b00cfcf617628082575aae1d41b3b2f80
dgpetrie <dpetrie@sipez.com>
2022-09-20 16:26:44 -0400

    Unit tests for UUID genration and setting

---
+++ tests/test_uuid.py
@@ -0,0 +1,37 @@
+"""
+unit tests for UUID generation and setting of uuid parameter
+"""
+
+import pytest
+import vcon
+
+def test_uuid8_time() -> None:
+
+  uuid = vcon.Vcon.uuid8_time(0)
+  assert(uuid[8] == '-')
+  assert(uuid[13] == '-')
+  assert(uuid[18] == '-')
+  assert(uuid[23] == '-')
+  assert(uuid[14] == '8')
+  assert(uuid[18:] == '-8000-000000000000')
+
+  print("uuid null rand_b: {}".format(uuid))
+
+def test_uuid8_domain_name() -> None:
+  uuid = vcon.Vcon.uuid8_domain_name("example.com")
+  print(uuid)
+
+def test_vcon_uuid() -> None:
+  new_vcon = vcon.Vcon()
+  new_vcon.add_uuid("example.com")
+  print("{} len: {}".format(new_vcon._vcon_dict["uuid"], len(new_vcon._vcon_dict[vcon.Vcon.UUID])))
+  assert(len(new_vcon._vcon_dict[vcon.Vcon.UUID]) == 36)
+
+  # Test serialization
+  copy_vcon = vcon.Vcon()
+  copy_vcon.loads(new_vcon.dumps())
+  assert(len(copy_vcon._vcon_dict[vcon.Vcon.UUID]) == 36)
+  assert(copy_vcon._vcon_dict[vcon.Vcon.UUID] == new_vcon._vcon_dict[vcon.Vcon.UUID])
+
+
+

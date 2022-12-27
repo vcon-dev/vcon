d3d351a7b8cd4cb940f4dca741d225ebd1978466
dgpetrie <dpetrie@sipez.com>
2022-12-27 17:50:57 -0500

    Pay attentionn to the HOSTNAME config parameter for binding host and port with uvicorn.

--- server/init.py
+++ server/init.py
@@ -1,7 +1,13 @@
 import uvicorn
 import logging
+from settings import HOSTNAME
+import urllib
 
 logging.config.fileConfig('./logging.conf')
 
 if __name__ == "__main__":
-    uvicorn.run("conserver:app", host="0.0.0.0", port=8000, reload=True)
+    url_parser = urllib.parse.urlparse(HOSTNAME)
+    host_ip = url_parser.hostname
+    port_num = url_parser.port
+    print("uvicorn binding to host: {} port: {}".format(host_ip, port_num))
+    uvicorn.run("conserver:app", host=host_ip, port=port_num, reload=True)

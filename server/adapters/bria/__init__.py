9d43092561579d8d565a4acd24b7891d96502e54
Pavan Kumar <pavan.kumar@strolid.com>
2023-01-10 10:07:06 +0000

    [#629] store and get vcon are moved into reusable lib
    Co-authored-by: sunaina <sunaina.khan@strolid.com>

--- server/adapters/bria/__init__.py
+++ server/adapters/bria/__init__.py
@@ -1,37 +1,50 @@
-import async_timeout
 import asyncio
-from datetime import datetime
+import hashlib
 import json
 import logging
 import logging.config
-import redis.asyncio as redis
-from settings import REDIS_URL, LOG_LEVEL, ENV
-from redis.commands.json.path import Path
-import vcon
-from dateutil.parser import parse
 import traceback
-import phonenumbers
+from datetime import datetime
 from typing import Optional
+import copy
+
+import async_timeout
 import boto3
-import hashlib
-from settings import AWS_KEY_ID, AWS_SECRET_KEY
+import phonenumbers
+import redis.asyncio as redis
+from dateutil.parser import parse
+from redis.commands.json.path import Path
+from settings import AWS_KEY_ID, AWS_SECRET_KEY, ENV, LOG_LEVEL, REDIS_URL
+
+import vcon
+from server.lib.vcon_redis import VconRedis
 
 logger = logging.getLogger(__name__)
 logger.setLevel(LOG_LEVEL)
-logger.info('Bria adapter loading')
+logger.info("Bria adapter loading")
 
 default_options = {
     "name": "bria",
     "ingress-list": [f"bria-conserver-feed-{ENV}"],
-    "egress-topics":["ingress-vcons"],
+    "egress-topics": ["ingress-vcons"],
 }
 
 
 def time_diff_in_seconds(start_time: str, end_time: str) -> int:
+    """Returns time difference in seconds for given start and end time
+
+    Args:
+        start_time (str): start time in iso string format (Z)
+        end_time (str): end time in iso string format (Z)
+
+    Returns:
+        int: number of seconds between start and end time
+    """
     start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%fZ")
     end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S.%fZ")
     duration = end_time - start_time
     return duration.seconds
+
 
 def compute_redis_key(body):
     dealer_number = get_e164_number(body.get("dialerId"))
@@ -41,28 +54,15 @@ def compute_redis_key(body):
 
 def add_bria_attachment(vCon, body, opts):
     # Set the adapter meta so we know where the this came from
-    adapter_meta= {
+    adapter_meta = {
         "adapter": "bria",
         "adapter_version": "0.1.0",
-        "src": opts['ingress-list'],
-        "type": 'call_completed',
+        "src": opts["ingress-list"],
+        "type": "call_completed",
         "received_at": datetime.now().isoformat(),
-        "payload": body
+        "payload": body,
     }
     vCon.attachments.append(adapter_meta)
-
-
-async def store_vcon(vCon, r):
-    key = f"vcon:{vCon.uuid}"
-    cleanvCon = json.loads(vCon.dumps())
-    await r.json().set(key, Path.root_path(), cleanvCon)
-
-
-async def get_vcon(vcon_id, r):
-    vcon_dict = await r.json().get(f"vcon:{vcon_id}", Path.root_path())
-    _vcon = vcon.Vcon()
-    _vcon.loads(json.dumps(vcon_dict))
-    return _vcon
 
 
 def get_party_index(vcon, tel=None, extension=None):
@@ -78,14 +78,14 @@ def get_party_index(vcon, tel=None, extension=None):
 
 
 def add_dialog(vcon, body):
-    start_time = body['startedAt']
-    end_time = body['endedAt']
+    start_time = body["startedAt"]
+    end_time = body["endedAt"]
     duration = time_diff_in_seconds(start_time, end_time)
 
     email = body.get("email")
-    username = email.split('@')[0]
-    first_name = username.split('.')[0]
-    last_name = username.split('.')[1]
+    username = email.split("@")[0]
+    first_name = username.split(".")[0]
+    last_name = username.split(".")[1]
     full_name = first_name + " " + last_name
     dealer_did = get_e164_number(body.get("dialerId"))
     customer_number = get_e164_number(body.get("customerNumber"))
@@ -93,45 +93,48 @@ def add_dialog(vcon, body):
 
     customer_index = get_party_index(vcon, tel=customer_number)
     if customer_index == -1:
-        customer_index = vcon.add_party({
-            "tel": customer_number,
-            "role": "customer",
-        })
-    agent_index = get_party_index(vcon, extension = extension)
+        customer_index = vcon.add_party(
+            {
+                "tel": customer_number,
+                "role": "customer",
+            }
+        )
+    agent_index = get_party_index(vcon, extension=extension)
     if agent_index == -1:
-        agent_index = vcon.add_party({
-            "tel": dealer_did,
-            "mailto": email,
-            "name":  full_name,
-            "role": "agent",
-            "extension": extension,
-        })
-    start_time = start_time.replace('Z', '+00:00')
+        agent_index = vcon.add_party(
+            {
+                "tel": dealer_did,
+                "mailto": email,
+                "name": full_name,
+                "role": "agent",
+                "extension": extension,
+            }
+        )
+    start_time = start_time.replace("Z", "+00:00")
     vcon.add_dialog_external_recording(
         body=None,
         start_time=start_time,
-        duration = duration,
-        parties = [customer_index, agent_index],
-        mime_type = "audio/x-wav",
-        file_name = f"{body['id']}.wav",
-        external_url = None
+        duration=duration,
+        parties=[customer_index, agent_index],
+        mime_type="audio/x-wav",
+        file_name=f"{body['id']}.wav",
+        external_url=None,
     )
 
 
-
-# async def handle_bria_call_started_event(body, r):
+# async def handle_bria_call_started_event(body, r, vcon_redis):
 #     logger.info("Processing call STARTED event %s", body)
 #     key = compute_redis_key(body)
 #     if (await r.exists(key)):
 #         await r.persist(key) # reset the expire timeout so that key stays there till call ends
 #     else:
 #         vCon = vcon.Vcon() # TODO what additional data we need to add here
-#         await store_vcon(vCon, r)
+#         await vcon_redis.store_vcon(vCon)
 #         await r.set(key, vCon.uuid) # NOTE what if we never receive call ended? The key will be infinite.
 #         await r.expire(key, 3600)
 
 
-async def handle_bria_call_ended_event(body, opts, r):
+async def handle_bria_call_ended_event(body, opts, r, vcon_redis):
     logger.info("Processing call ENDED event %s", body)
     # Construct empty vCon, set meta data
     redis_key = compute_redis_key(body)
@@ -141,17 +144,19 @@ async def handle_bria_call_ended_event(body, opts, r):
         vCon = vcon.Vcon()
         await r.set(redis_key, vCon.uuid)
     else:
-        vCon = await get_vcon(vcon_id, r)
+        vCon = await vcon_redis.get_vcon(vcon_id)
     logger.info(f"The vcon id is {vCon.uuid}")
     await r.expire(redis_key, 3600)
     add_dialog(vCon, body)
     add_bria_attachment(vCon, body, opts)
-    await store_vcon(vCon, r)
+    await vcon_redis.store_vcon(vCon)
     for egress_topic in opts["egress-topics"]:
         await r.publish(egress_topic, vCon.uuid)
 
 
-def create_presigned_url(bucket_name: str, object_key: str, expiration: int = 3600) -> str:
+def create_presigned_url(
+    bucket_name: str, object_key: str, expiration: int = 3600
+) -> str:
     """Generate a presigned URL to share an S3 object
 
     :param bucket_name: string
@@ -161,14 +166,17 @@ def create_presigned_url(bucket_name: str, object_key: str, expiration: int = 36
     """
 
     # Generate a presigned URL for the S3 object
-    s3_client = boto3.client('s3', aws_access_key_id=AWS_KEY_ID, aws_secret_access_key=AWS_SECRET_KEY)
+    s3_client = boto3.client(
+        "s3", aws_access_key_id=AWS_KEY_ID, aws_secret_access_key=AWS_SECRET_KEY
+    )
     try:
-        response = s3_client.generate_presigned_url('get_object',
-                                                    Params={'Bucket': bucket_name,
-                                                            'Key': object_key},
-                                                    ExpiresIn=expiration)
+        response = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": bucket_name, "Key": object_key},
+            ExpiresIn=expiration,
+        )
     except Exception as e:
-        logging.error(e) # TODO should we rethrow this error???
+        logging.error(e)  # TODO should we rethrow this error???
         return None
 
     # The response contains the presigned URL
@@ -176,45 +184,71 @@ def create_presigned_url(bucket_name: str, object_key: str, expiration: int = 36
 
 
 def create_sha512_hash_for_s3_file(bucket_name: str, object_key: str) -> str:
-    s3 = boto3.client('s3', aws_access_key_id=AWS_KEY_ID, aws_secret_access_key=AWS_SECRET_KEY)
+    s3 = boto3.client(
+        "s3", aws_access_key_id=AWS_KEY_ID, aws_secret_access_key=AWS_SECRET_KEY
+    )
     # Retrieve the file from S3 and calculate the SHA-512 fingerprint
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
-    contents = response['Body'].read()
+    contents = response["Body"].read()
     fingerprint = hashlib.sha512(contents).hexdigest()
     return fingerprint
 
 
-HUNDREAD_YEARS_SECONDS = 3.156e+8
-async def handle_bria_s3_recording_event(record, opts, r):
+HUNDREAD_YEARS_SECONDS = 3.156e8
+
+
+async def handle_bria_s3_recording_event(record, opts, redis_client, vcon_redis):
+    """Called when new s3 event is received. This function will update dialog object in vcon with correct url for recording and hash checksum
+
+    Args:
+        record (s3 event): s3 event object
+        opts (dict): _description_
+        r (redis_client): Redis client
+        vcon_redis (VconRedis): Instance of vcon redis
+    """
     logger.info("Processing s3 recording %s", record)
     s3_object_key = record["s3"]["object"]["key"]
     s3_bucket_name = record["s3"]["bucket"]["name"]
-    bria_call_id = s3_object_key.replace('.wav', '')
+    bria_call_id = s3_object_key.replace(".wav", "")
     # lookup the vCon in redis using this ID
     # FT.SEARCH idx:adapterIdIndex '@adapter:{bria} @id:{f8be045704cb4ea98d73f60a88590754}'
-    result = await r.ft(index_name="idx:adapterIdsIndex").search(f"@adapter:{{bria}} @id:{{{bria_call_id}}}")
-    vCon = vcon.Vcon()
-    vCon.loads(result.docs[0].json)
-    for index, d in enumerate(vCon.dialog):
-        if d.get("filename") == f"{bria_call_id}.wav":
+    result = await redis_client.ft(index_name="idx:adapterIdsIndex").search(
+        f"@adapter:{{bria}} @id:{{{bria_call_id}}}"
+    )
+    v_con = vcon.Vcon()
+    v_con.loads(result.docs[0].json)
+    for index, dialog in enumerate(v_con.dialog):
+        if dialog.get("filename") == f"{bria_call_id}.wav":
             # TODO https:// find a way to get https link with permenent access
-            vCon.dialog[index]["url"] = create_presigned_url(s3_bucket_name, s3_object_key, HUNDREAD_YEARS_SECONDS)
-            vCon.dialog[index]['signature'] = create_sha512_hash_for_s3_file(s3_bucket_name, s3_object_key)
-            vCon.dialog[index]['alg'] = "SHA-512"
-    await store_vcon(vCon, r)
+            v_con.dialog[index]["url"] = create_presigned_url(
+                s3_bucket_name, s3_object_key, HUNDREAD_YEARS_SECONDS
+            )
+            v_con.dialog[index]["signature"] = create_sha512_hash_for_s3_file(
+                s3_bucket_name, s3_object_key
+            )
+            v_con.dialog[index]["alg"] = "SHA-512"
+    await vcon_redis.store_vcon(v_con)
     for egress_topic in opts["egress-topics"]:
-        await r.publish(egress_topic, vCon.uuid)
+        await redis_client.publish(egress_topic, v_con.uuid)
 
 
-async def start(opts=default_options):
+async def start(opts=None):
+    """Starts the bria adaptor as co-routine. This adaptor will run till it's killed.
+
+    Args:
+        opts (Defalut options, optional): Options which controls behaviour of this adaptor. Defaults to None.
+    """
+    if opts is None:
+        opts = copy.deepcopy(default_options)
     logger.info("Starting the bria adapter")
     # Setup redis
     r = redis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
+    vcon_redis = VconRedis(redis_client=r)
     while True:
         # logger.info("Bria adaptor loop")
         try:
             # Terminate if it takes longer than 10 sec
-            async with async_timeout.timeout(10):  
+            async with async_timeout.timeout(10):
                 for ingress_list in opts["ingress-list"]:
                     data = await r.lpop(ingress_list)
                     if data is None:
@@ -224,14 +258,18 @@ async def start(opts=default_options):
                     records = payload.get("Records")
                     if records:
                         for record in records:
-                            await handle_bria_s3_recording_event(record, opts, r)
+                            await handle_bria_s3_recording_event(
+                                record, opts, r, vcon_redis
+                            )
                     else:
                         body = json.loads(payload.get("Message"))
                         event_type = payload["MessageAttributes"]["kind"]["Value"]
                         if event_type == "call_ended":
-                            await handle_bria_call_ended_event(body, opts, r)
+                            await handle_bria_call_ended_event(
+                                body, opts, r, vcon_redis
+                            )
                         # elif event_type == "call_started":
-                        #     await handle_bria_call_started_event(body, r)
+                        #     await handle_bria_call_started_event(body, r, vcon_redis)
                         else:
                             logger.info(f"Ignoring the Event Type : {event_type}")
         except asyncio.CancelledError:
@@ -244,8 +282,16 @@ async def start(opts=default_options):
 
 
 def get_e164_number(phone_number: Optional[str]) -> str:
+    """Returns the phone number in E164 format
+
+    Args:
+        phone_number (Optional[str]): the phone number to be formated to E164
+
+    Returns:
+        str: phone number formated in E164 format
+    """
     if not phone_number:
-        return ''
+        return ""
     parsed = phonenumbers.parse(phone_number, "US")
     the_return = phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.E164)
     logger.info("The return %s", the_return)
---
+++ server/lib/vcon_redis.py
@@ -0,0 +1,46 @@
+import redis.asyncio as redis
+from redis.commands.json.path import Path
+import vcon
+import json
+from typing import Optional
+
+
+class VconRedis:
+    """Encapsulate vcon redis operation
+    """
+
+    def __init__(self, redis_url=None, redis_client=None):
+        if redis_url:
+            self._redis_client = redis.from_url(redis_url, encoding="utf-8", decode_responses=True)
+        elif redis_client:
+            self._redis_client = redis_client
+        else:
+            raise Exception("Aurguments for VconRedis missing. redis_url or redis_client is required.")
+
+
+    async def store_vcon(self, vCon: vcon.Vcon):
+        """Stores the vcon into redis
+
+        Args:
+            vCon (vcon.Vcon): this vCon gets stored in redis
+        """
+        key = f"vcon:{vCon.uuid}"
+        cleanvCon = json.loads(vCon.dumps())
+        await self._redis_client.json().set(key, Path.root_path(), cleanvCon)
+
+
+    async def get_vcon(self, vcon_id: str) -> Optional[vcon.Vcon]:
+        """Retrives the vcon from redis for given vcon_id
+
+        Args:
+            vcon_id (str): vcon id for which needs to be retrived from redis
+
+        Returns:
+            Optional[vcon.Vcon]: Retirns vcon for givin vcon id or None if vcon is not present.
+        """
+        vcon_dict = await self._redis_client.json().get(f"vcon:{vcon_id}", Path.root_path())
+        if not vcon_dict:
+            return None
+        _vcon = vcon.Vcon()
+        _vcon.loads(json.dumps(vcon_dict))
+        return _vcon

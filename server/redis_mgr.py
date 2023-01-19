123f3f3d643a5c4bb54ba1f86a7753179e9ff6d1
dgpetrie <dpetrie@sipez.com>
2023-01-19 20:26:33 +0000

    Created redis connection pool and client manager to clean up old clients. If FastApi gets restarted (e.g. in unit tests) some modules were stuck with old redis clients running in a non-existing asyncio event loop.  This caused things to hand or raise exceptions.  This module manages the connections to avoid this problem.  Some modules have been migrated to use this, but others remain that still need to be converted.

---
+++ server/redis_mgr.py
@@ -0,0 +1,66 @@
+""" 
+Package to manage Redis connection pool and clients
+
+Setup of redis clients cannot be done globally in each module as it will 
+bind to a asyncio loop which may be started and stopped.  In which case
+redis will be bound to an old loop which will no longer work
+
+The redis connection pool must be shutdown and restarted when FASTApi does.
+"""
+
+import logging
+import redis.asyncio.connection
+import redis.asyncio.client
+from settings import REDIS_URL
+
+import traceback
+import sys
+
+logger = logging.getLogger(__name__)
+
+REDIS_POOL = None
+REDIS_POOL_INITIALIZATION_COUNT = 0
+
+def create_pool():
+  global REDIS_POOL
+  global REDIS_POOL_INITIALIZATION_COUNT
+  if(REDIS_POOL is not None):
+    logger.error("Redis pool already created")
+  else:
+    REDIS_POOL_INITIALIZATION_COUNT += 1
+    REDIS_POOL = redis.asyncio.connection.ConnectionPool.from_url(REDIS_URL)
+    logger.info('redis connection: host: {} port: {} max connections: {} initialization count: {}'.format(
+      REDIS_POOL.connection_kwargs.get("host", "None"),
+      REDIS_POOL.connection_kwargs.get("port", "None"),
+      REDIS_POOL.max_connections,
+      REDIS_POOL_INITIALIZATION_COUNT))
+
+  logger.info(dir(REDIS_POOL))
+
+async def shutdown_pool():
+  global REDIS_POOL
+  if(REDIS_POOL is not None):
+    logger.info("disconnecting Redis pool")
+    tmp_pool = REDIS_POOL
+    REDIS_POOL = None
+    await tmp_pool.disconnect(inuse_connections=True)
+
+  else:
+    logger.error("Redis pool already disconnected")
+
+def get_client():
+  global REDIS_POOL
+  if(REDIS_POOL is None):
+    #traceback.print_exc()
+    print("Error: redis pool not initialized", file=sys.stdout)
+    traceback.print_stack(file=sys.stdout)
+    logger.error("redis connection pool not initialized")
+    e = Exception("Redis pool not initialized")
+    raise e
+
+  logger.info("getting redis connection")
+  r = redis.asyncio.client.Redis(connection_pool=REDIS_POOL)
+  #r = redis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
+
+  return(r)
+

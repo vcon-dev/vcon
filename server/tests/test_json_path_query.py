1f1b2f35a3d72edf64c8f2f8e3922fd875d905b0
dgpetrie <dpetrie@sipez.com>
2023-01-21 00:53:39 +0000

    Added unit test for JSONPath entry point

---
+++ server/tests/test_json_path_query.py
@@ -0,0 +1,37 @@
+
+import os
+import logging
+import json
+import fastapi.testclient
+import pytest
+
+import conserver
+import vcon
+import conserver_test
+
+logging.config.fileConfig('tests/logging.conf')
+logger = logging.getLogger(__name__)
+
+
+#@pytest.mark.incremental
+@pytest.mark.dependency
+#@pytest.mark.dependency(depends=["test_1_post_dialog_vcon"])
+def test_2_json_path():
+  logger.debug("Starting test_2_json_path")
+  with fastapi.testclient.TestClient(conserver.app) as client:
+    query = {}
+    query["path_string"] = "$.dialog[0]"
+    response = client.get("/vcon/{}/JSONPath".format(conserver_test.UUID), params=query)
+    assert(response.status_code == 200)
+    print("text: " + response.text)
+    print("response dir: {}".format(dir(response)))
+
+    dialog_array = json.loads(response.text)
+    assert(len(dialog_array) == 1)
+    assert(response.text == str(response.content, "utf-8"))
+    assert(dialog_array[0]["url"] == conserver_test.url)
+    assert(dialog_array[0]["mimetype"] == vcon.Vcon.MIMETYPE_AUDIO_WAV)
+    assert(dialog_array[0]["filename"] == os.path.basename(conserver_test.file_path))
+
+  logger.debug("Exiting test_2_json_path")
+

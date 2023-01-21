00560b6bed8cda32ff653733748ba041944a3837
dgpetrie <dpetrie@sipez.com>
2023-01-21 00:52:52 +0000

    Use conserver_test data in test_post_dialog

--- server/tests/test_post_dialog.py
+++ server/tests/test_post_dialog.py
@@ -1,22 +1,18 @@
+
 import os
-import pytest
 import logging
-import fastapi.testclient
 import datetime
 import json
 
+import fastapi.testclient
+import pytest
+
 import conserver
 import vcon
+import conserver_test
 
 logging.config.fileConfig('tests/logging.conf')
 logger = logging.getLogger(__name__)
-
-UUID = "01855517-ac4e-8edf-84fd-77776666acbe"
-
-file_path = "../examples/agent_sample.wav"
-
-url = "https://github.com/vcon-dev/vcon/blob/main/examples/agent_sample.wav?raw=true"
-
 
 #@pytest.mark.incremental
 @pytest.mark.dependency
@@ -29,7 +25,7 @@ def test_1_post_dialog_vcon():
     second_party = vCon.set_party_parameter("tel", "5678")
 
     file_content = b""
-    with open(file_path, "rb") as file_handle:
+    with open(conserver_test.file_path, "rb") as file_handle:
       file_content = file_handle.read()
       print("body length: {}".format(len(file_content)))
       assert(len(file_content) > 10000)
@@ -38,13 +34,13 @@ def test_1_post_dialog_vcon():
       datetime.datetime.utcnow(),
       0, # duration TODO
       [0,1],
-      url,
+      conserver_test.url,
       vcon.Vcon.MIMETYPE_AUDIO_WAV,
-      os.path.basename(file_path))
+      os.path.basename(conserver_test.file_path))
     # hack the UUID so we have a predictable UUID and we don't polute the DB
-    vCon._vcon_dict["uuid"] = UUID
+    vCon._vcon_dict["uuid"] = conserver_test.UUID
     uuid = vCon.uuid
-    assert(uuid == UUID)
+    assert(uuid == conserver_test.UUID)
     vcon_json_string = vCon.dumps()
     vcon_json_object = json.loads(vcon_json_string)
     print("created vcon: {}".format(vcon_json_object))
@@ -59,7 +55,7 @@ def test_1_post_dialog_vcon():
     assert(saved_vcon.parties[0]["tel"] == "1234")
     assert(saved_vcon.parties[1]["tel"] == "5678")
     assert(len(saved_vcon.dialog) == 1)
-    assert(saved_vcon.dialog[0]["url"] == url)
+    assert(saved_vcon.dialog[0]["url"] == conserver_test.url)
     assert(len(saved_vcon.analysis) == 0)
 
   logger.debug("Exiting test_1_post_dialog_vcon")

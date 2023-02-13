83dc400019dd0b08539a0b5b5fae16776c2565ff
Pavan Kumar <pavan.kumar@strolid.com>
2023-02-13 11:31:37 +0000

    [#633] Make default stream std out
    Co-authored-by: sergey <sergey.lyapustin@strolid.com>

--- server/lib/logging_utils.py
+++ server/lib/logging_utils.py
@@ -2,11 +2,12 @@ import logging
 import logging.config
 from pythonjsonlogger import jsonlogger
 from settings import LOG_LEVEL
+import sys
 
 def init_logger(name):
     logger = logging.getLogger(name)
     formatter = jsonlogger.JsonFormatter('%(timestamp)s %(levelname)s %(message)s ', timestamp=True)
-    logHandler = logging.StreamHandler()
+    logHandler = logging.StreamHandler(sys.stdout)
     logHandler.setFormatter(formatter)
     logger.addHandler(logHandler)
     logger.setLevel(LOG_LEVEL)

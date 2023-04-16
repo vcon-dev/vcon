5a141e571abe850cdac2a346707071a894cf5a04
Thomas Howe <howethomas@users.noreply.github.com>
2023-04-16 19:09:55 -0400

    Fixes unit tests for redis_mgr.
    Adds pool support to fix broken tests.

--- tests/test_redis_mgr.py
+++ tests/test_redis_mgr.py
@@ -5,6 +5,7 @@ import pytest
 import random
 import string
 sys.path.append("server")
+import server.redis_mgr as redis_mgr
 from server.redis_mgr import set_key, get_key, delete_key
 
 def get_random_string(length):
@@ -15,6 +16,7 @@ def get_random_string(length):
 
 @pytest.mark.asyncio
 async def test_get_and_read():
+    redis_mgr.create_pool()
     print(sys.path)
     random_bytes = get_random_string(10)
     random_key = get_random_string(10)
@@ -23,17 +25,22 @@ async def test_get_and_read():
     assert(read_back==random_bytes)
     await delete_key(random_key)
     result = await get_key(random_key)
+    await redis_mgr.shutdown_pool()
     assert(result==None)
 
 @pytest.mark.asyncio
 async def test_unit_key():
+    redis_mgr.create_pool()
     random_key = get_random_string(10)
     result = await get_key(random_key)
+    await redis_mgr.shutdown_pool()
     assert(result==None)
 
 @pytest.mark.asyncio
 async def test_empty_key():
     try:
+        redis_mgr.create_pool()
         result = await get_key("")
+        await redis_mgr.shutdown_pool()
     except Exception as e:
         assert(type(e)==TypeError)

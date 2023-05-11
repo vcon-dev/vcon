8e0a8edf49735f273ad0f8e21919f9e0016b6677
Pavan Kumar <pavan.kumar@strolid.com>
2023-05-11 11:52:45 +0000

    Fix webhook payload
    Co-authored-by: sergey <sergey.lyapustin@strolid.com>
    Co-authored-by: sunaina <sunaina.khan@strolid.com>
    Co-authored-by: Thomas Howe <howethomas@users.noreply.github.com>

--- server/links/webhook/__init__.py
+++ server/links/webhook/__init__.py
@@ -2,6 +2,7 @@ from server.lib.vcon_redis import VconRedis
 from lib.logging_utils import init_logger
 logger = init_logger(__name__)
 import aiohttp
+import json
 
 default_options = {
     "webhook-urls": ["https://eo91qivu6evxsty.m.pipedream.net"],
@@ -19,11 +20,12 @@ async def run(
 
     # The webhook needs a stringified JSON version. 
     json_vCon = vCon.dumps()
+    json_dict = json.loads(json_vCon)
     
     # Post this to each webhook url
     for url in opts["webhook-urls"]:
         async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=json_vCon) as resp:
+            async with session.post(url, json=json_dict) as resp:
                 logger.debug("webhook plugin: posted to webhook url: {}".format(url))
                 logger.debug("webhook plugin: response: {}".format(resp.status))
     # Return the vcon_uuid down the chain.

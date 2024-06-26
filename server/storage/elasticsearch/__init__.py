915e22562259ef79f5357907b6bbce39ee8e061c
Pavan Kumar <pavan.kumar@strolid.com>
2024-06-26 08:53:48 +0000

    ENG-882 Indexed start_at time so that we could sort result, also refactored
    Co-authored-by: sergey <sergey.lyapustin@strolid.com>

--- server/storage/elasticsearch/__init__.py
+++ server/storage/elasticsearch/__init__.py
@@ -17,11 +17,24 @@ default_options = {
 }
 
 
+def do_vcon_parts_indexing(*, es, part, index_name, id, common_attributes,):
+    new_part = {**part, **common_attributes}
+    try:
+        es.index(
+            index=index_name,
+            id=id,
+            document=new_part,
+        )
+    except Exception as e:
+        logger.error(
+            f"Elasticsearch storage plugin: failed to insert: {new_part}, error: {e} ", exc_info=True
+        )
+
+
 def save(
     vcon_uuid,
     opts=default_options,
 ):
-    logger.info("Starting the Elasticsearch storage for vCon: %s", vcon_uuid)
     try:
         es = Elasticsearch(
             cloud_id=opts["cloud_id"],
@@ -31,97 +44,66 @@ def save(
         vcon = vcon_redis.get_vcon(vcon_uuid)
         vcon_dict = vcon.to_dict()
 
+        started_at = vcon_dict["dialog"][0]["start"]
+        
+        common_attributes = {
+            "vcon_id": vcon_uuid,
+            "started_at": started_at,
+        }
+
         tenant_attachment = vcon.find_attachment_by_type("tenant")
-        tenant_id = None
         if tenant_attachment:
             tenant = json.loads(tenant_attachment["body"])
-            tenant_id = tenant["id"]
-
-        # es.index(
-        #     index=opts["index"],
-        #     id=vcon_dict["uuid"],
-        #     document=vcon_dict,
-        # )
+            common_attributes["tenant_id"] = tenant["id"]
 
         # Index the parties, separated by 'role' - id=f"{vcon_uuid}_{party_index}"
         for ind, party in enumerate(vcon_dict["parties"]):
             role = party.get("meta", {}).get("role")
-            index_name = "vcon_parties"
-            if role:
-                index_name += f"_{role}"
-            if tenant_id:
-                party["tenant_id"] = tenant_id
-            party["vcon_id"] = vcon_dict["uuid"]
-            try:
-                es.index(
-                    index=index_name,
-                    id=f"{vcon_dict['uuid']}_{ind}",
-                    document=party,
-                )
-            except Exception as e:
-                logger.error(
-                    f"Elasticsearch storage plugin: failed to insert party: {party}, error: {e} ", exc_info=True
-                )
+            do_vcon_parts_indexing(
+                es=es,
+                part=party, 
+                index_name=f"vcon_parties_{role}" if role else "vcon_parties", 
+                id=f"{vcon_uuid}_{ind}", 
+                common_attributes=common_attributes
+            )
 
         # Index the attachments, separated by 'type' - id=f"{vcon_uuid}_{attachment_index}"
         for ind, attachment in enumerate(vcon_dict["attachments"]):
             type = attachment.get("type")  # TODO this might be "purpose" in some of the attachments!!
-            index_name = f"vcon_attachments_{type}"
             if attachment["encoding"] == "json":  # TODO may be we need handle different encodings
                 attachment["body"] = json.loads(attachment["body"])
-            if tenant_id:
-                attachment["tenant_id"] = tenant_id
-            attachment["vcon_id"] = vcon_dict["uuid"]
-            try:
-                es.index(
-                    index=index_name,
-                    id=f"{vcon_dict['uuid']}_{ind}",
-                    document=attachment,
-                )
-            except Exception as e:
-                logger.error(
-                    f"Elasticsearch storage plugin: failed to insert attachment: {attachment}, error: {e} ", exc_info=True
-                )
+            do_vcon_parts_indexing(
+                es=es,
+                part=attachment, 
+                index_name=f"vcon_attachments_{type}", 
+                id=f"{vcon_dict['uuid']}_{ind}", 
+                common_attributes=common_attributes
+            )
 
         # Index the analysis, separated by 'type' - id=f"{vcon_uuid}_{analysis_index}"
         for ind, analysis in enumerate(vcon_dict["analysis"]):
             type = analysis.get("type")
-            index_name = f"vcon_analysis_{type}"
             if analysis["encoding"] == "json":  # TODO may be we need handle different encodings
                 if isinstance(analysis["body"], str):
                     analysis["body"] = json.loads(analysis["body"])
-            if tenant_id:
-                analysis["tenant_id"] = tenant_id
-            analysis["vcon_id"] = vcon_dict["uuid"]
-            try:
-                es.index(
-                    index=index_name,
-                    id=f"{vcon_dict['uuid']}_{ind}",
-                    document=analysis,
-                )
-            except Exception as e:
-                logger.error(
-                    f"Elasticsearch storage plugin: failed to insert analysis: {analysis}, error: {e} ", exc_info=True
-                )
+            do_vcon_parts_indexing(
+                es=es,
+                part=analysis, 
+                index_name=f"vcon_analysis_{type}", 
+                id=f"{vcon_dict['uuid']}_{ind}", 
+                common_attributes=common_attributes
+            )
 
         # Index the dialog - id=f"{vcon_uuid}_{dialog_index}"
         # TODO: Consider separate indexes for different dialog 'types'
         for ind, dialog in enumerate(vcon_dict["dialog"]):
-            if tenant_id:
-                dialog["tenant_id"] = tenant_id
-            dialog["vcon_id"] = vcon_dict["uuid"]
-            try:
-                es.index(
-                    index="vcon_dialog",
-                    id=f"{vcon_dict['uuid']}_{ind}",
-                    document=dialog,
-                )
-            except Exception as e:
-                logger.error(
-                    f"Elasticsearch storage plugin: failed to insert dialog: {dialog}, error: {e} ", exc_info=True
-                )
-                
-        logger.info("Finished the Elasticsearch storage for vCon: %s", vcon_uuid)
+            do_vcon_parts_indexing(
+                es=es,
+                part=dialog, 
+                index_name="vcon_dialog", 
+                id=f"{vcon_dict['uuid']}_{ind}", 
+                common_attributes=common_attributes
+            )
     except Exception as e:
         logger.error(
             f"Elasticsearch storage plugin: failed to insert vCon: {vcon_uuid}, error: {e} ", exc_info=True

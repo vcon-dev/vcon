ccbbc19bb8043b1f6b7ad5894699526bd837db4c
Thomas Howe <ghostofbasho@gmail.com>
2024-07-31 20:16:31 -0400

    Brings the various repositories back to one place.

--- server/lib/logging_utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-from settings import LOGGING_CONFIG_FILE
-import logging.config
-
-logging.config.fileConfig(LOGGING_CONFIG_FILE)
-
-
-def init_logger(name):
-    return logging.getLogger(name)
